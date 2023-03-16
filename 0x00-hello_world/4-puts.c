b46b817857e334111913e0a52cdd7bc3ed24d275
Travis Munuku <travismunuku10154@gmail.com>
2023-03-16 13:33:54 -0700

    new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,13 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle
  * Return 0.
- */	
+ */
 
 
 int main (void)
 
-{	
-	puts ("\"Programming is like building a multilingual puzzle");
+{
+puts ("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
