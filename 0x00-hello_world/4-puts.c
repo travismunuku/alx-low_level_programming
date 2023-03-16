4c438f469159e9ba4ce42ae5a00e2f015f589ca1
Travis Munuku <travismunuku10154@gmail.com>
2023-03-16 12:46:53 -0700

    hotfix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 # include <stdio.h>
+
+
 /**
- * main -Program that prints a line using the puts function 
- * *
- * Return: 0
- */			
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return 0.
+ */	
+
+
 int main (void)
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+
+{	
+	puts ("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
