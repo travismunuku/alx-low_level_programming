ba2e6ae47441825ebd9d505d626e17cad78f170a
Travis Munuku <travismunuku10154@gmail.com>
2023-03-16 14:09:01 -0700

    new updated file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - "with proper grammar, but the outcome is a piece of art"
+ * followed by a newline
+ * Return: Always 0
+ */
+
+int main (void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
+
