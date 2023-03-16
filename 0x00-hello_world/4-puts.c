1ed9f7fa4e888b9a3c6ac45e667c2c5eb68a8c0f
Travis Munuku <travismunuku10154@gmail.com>
2023-03-16 10:31:42 -0700

    a new updated file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+/**
+ * main -Program that prints a line using the puts function 
+ * *
+ * Return: 0
+ */			
+int main (void)
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
