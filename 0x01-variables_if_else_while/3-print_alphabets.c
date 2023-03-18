79a7569f7ff132689fb07ae650031273d6cf7114
Travis Munuku <travismunuku10154@gmail.com>
2023-03-17 19:29:10 -0700

    new file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints uppercase alphebtical letters
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+        char low;
+
+        for (low = 'a'; low <= 'Z'; low++)
+        putchar(low);
+
+	for (low ='A'; low <= 'Z'; low++)
+	putchar(low);
+        putchar('\n');
+
+        return (0);
+
+}
