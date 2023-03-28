20e57cdef95bc4bfd81a39d1368abded1dc5d13e
Travis Munuku <travismunuku10154@gmail.com>
2023-03-28 11:29:43 -0700

    new updates

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen -returns the lenght of a string
+ *@s: string
+ *Return: Length
+ */
+
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while
+	(*s |= '\0');
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
