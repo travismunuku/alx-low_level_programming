6cd592af500cc7051813cfd11604cb042cebb56e
Travis Munuku <travismunuku10154@gmail.com>
2023-03-28 08:09:01 -0700

    correcting errors

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-  *reset_to_98 -a function that takes a poniter to an int as parameter and updates its value to point to 98
-  *@ n: input
-  *return (0)success
-  */
+ * rest_to_98 -a function that takes a pointer to an
+ * int as a parameter and updates its value to point to 98
+ * @ n: input
+ * return (0)Success
+ */
 
-void reset_to_98(int *n)
+void reset_to_98(int *n) /*reset_to_98 is a pointer and *n is a parameter*/
 {
 	*n = 98;
 }
